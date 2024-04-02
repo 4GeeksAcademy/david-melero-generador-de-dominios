fee89a1b55a04ed5afed299d0685aef089e7c213
David <149296106+dmeleroandrade@users.noreply.github.com>
2024-04-02 14:09:58 +0000

    final

--- src/app.js
+++ src/app.js
@@ -4,52 +4,58 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
   let domain = [".com", ".net"];
 
-  function generatePhrasesRandom() {
-    const names = [pronoun, adj, noun];
-    const randomWords = names.map(item => {
-      let numberRandom = Math.floor(Math.random() * item.length);
-      return item[numberRandom];
+  let textFor = '<ul class="list-group">';
+  textFor += '<li class="list-group-item bg-success text-light">With for </li>';
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let h = 0; h < noun.length; h++) {
+        for (let k = 0; k < domain.length; k++) {
+          textFor +=
+            '<li class="list-group-item bg-primary text-light">' +
+            pronoun[i] +
+            adj[j] +
+            noun[h] +
+            domain[k] +
+            "</li>";
+        }
+      }
+    }
+  }
+  textFor += "</ul>"; // Corregido el cierre de la etiqueta ul
+  document.querySelector("#for").innerHTML = textFor;
+
+  // Aquí utilizo el for of
+  let textOf = '<ul class="list-group">';
+  textOf +=
+    '<li class="list-group-item bg-success text-light">With for of </li>';
+
+  for (const item of pronoun) {
+    for (const element of adj) {
+      for (const iterator of noun) {
+        for (const ele of domain) {
+          textOf += `
+                  <li class="list-group-item">
+                   ${item}${element}${iterator}${ele}
+                   </li>`;
+        }
+      }
+    }
+  }
+  textOf += "</ul>";
+  document.querySelector("#of").innerHTML = textOf;
+
+  // Aquí utilizo .map()
+  let textMap = '<ul class="list-group">';
+  textMap += '<li class="list-group-item bg-success text-light">With map </li>';
+  pronoun.forEach(item => {
+    adj.forEach(element => {
+      noun.forEach(iterator => {
+        domain.forEach(ele => {
+          textMap += `<li class= "list-group-item">${item}${element}${iterator}${ele}</li>`;
+        });
+      });
     });
-
-    const randomDomain = domain[Math.floor(Math.random() * domain.length)];
-    return randomWords.join("") + randomDomain;
-  }
-
-  function updatePhrase() {
-    let random = generatePhrasesRandom();
-    const textHTML = document.querySelector("#title");
-    textHTML.innerHTML = random;
-  }
-  updatePhrase();
+  });
+  textMap += "</ul>";
+  document.querySelector("#map").innerHTML = textMap;
 };
-
-// window.onload = function () {
-
-//     let pronoun = ['the', 'our'];
-//     let adj = ['great', 'big'];
-//     let noun = ['jogger', 'racoon'];
-//     let domain = ['.com', '.net', '.es']
-
-//     // let pronounindex = Math.floor(Math.random() * pronoun.length)
-//     // let adjindex = Math.floor(Math.random() * pronoun.length)
-//     // let nounindex = Math.floor(Math.random() * pronoun.length)
-//     let names = ['pronoun', 'adj', 'noun']
-//     let generatePhrases = ' '
-//     for (let i = 0; i < names.length; i++) {
-//         let indexRandom;
-//         if (names[i] === 'pronoun') {
-//             indexRandom = Math.floor(Math.random() * pronoun.length)
-//             generatePhrases += (pronoun[indexRandom])
-//         } else if (names[i] === 'adj') {
-//             indexRandom = Math.floor(Math.random() * adj.length)
-//             generatePhrases += (adj[indexRandom])
-//         } else if (names[i] === 'noun') {
-//             indexRandom = Math.floor(Math.random() * noun.length)
-//             generatePhrases += (noun[indexRandom])
-//         }
-//     }
-//     let domainText = ''
-
-//         const phrases = document.querySelector('#title')
-//     phrases.innerHTML = generatePhrases
-// };
